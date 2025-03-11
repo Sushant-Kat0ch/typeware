ad720e20e82c6063f2625e39bc2aa02abaf2ed36
Sushant-Kat0ch <sushantkatoch16@gmail.com>
2025-03-11 06:49:33 +0530

    Added cursor and word text logic

--- src/app/pages/dashboard/components/paragraphType.ts
+++ src/app/pages/dashboard/components/paragraphType.ts
@@ -7,12 +7,16 @@ import { CommonModule } from '@angular/common';
     imports: [CommonModule],
     template: `<div class="col-span-12 lg:col-span-6 xl:col-span-3">
             <div class="card mb-0">
-                <div class="mb-4 text-lg" [innerHTML]="'<p>' + currentPara.join('') + '</p>'">
+                <div class="mb-4 text-xl line-text " [innerHTML]="'<p>' + currentPara.join('') + '</p>'">
                     <p>{{currentPara.join('')}}</p>
         </div>`,
+    styles: [`
+    .line-text { font-size: 1.8em; line-height: 1.5; }
+    .bottom-cursor{ text-decoration: underline; text-decoration-color: #f9a8d4; }
+    `]
 })
 export class ParaType implements OnInit {
-    public paragraphs: string[] = ["Life is a collection of moments.  Don't get so caught up in the past or worried about the future that you miss the beauty and opportunity of the present. Practice mindfulness, savor experiences, and appreciate the small joys that each day offers.  Focusing on the now can reduce stress and increase overall happiness."]
+    public paragraphs: string[] = ["Life is a collection of moments. Don't get so caught up in the past or worried about the future that you miss the beauty and opportunity of the present. Practice mindfulness, savor experiences, and appreciate the small joys that each day offers.  Focusing on the now can reduce stress and increase overall happiness."]
     public currentPara: string[] = this.paragraphs[0].split('').map(key => `<span class="inline-block default">${key == ' ' ? '&nbsp' : key.toLowerCase() }</span>`)
     currentKey:string[] = this.paragraphs[0].split('')
     key: string = '';
@@ -22,17 +26,26 @@ export class ParaType implements OnInit {
     @HostListener('window:keydown', ['$event'])
     onKeyDown(event: KeyboardEvent) {
         this.key = event.key; // Store the key that was pressed down.
-        if (this.key === this.currentKey[this.typed()].toLocaleLowerCase()) { // Compare with current key
+        if(this.key === 'Backspace') {
+            this.currentPara[this.typed()] = `<span class="default">${this.currentKey[this.typed()] == ' ' ? '&nbsp' : this.currentKey[this.typed()].toLowerCase() }</span>`
+            this.typed.update(value => value -1); // Decrement the signal.
+          }
+          else if (this.key === this.currentKey[this.typed()].toLocaleLowerCase()) { // Compare with current key
             console.log('Correct key pressed!');
             this.currentPara[this.typed()] = `<span class="text-green-300">${this.currentKey[this.typed()] == ' ' ? '&nbsp' : this.currentKey[this.typed()].toLowerCase() }</span>`  
             this.typed.update(value => value +1); // Increment the signal.
-            if (this.typed() === this.currentKey.length) {
-              console.log('All keys typed correctly!');
-              this.typed.set(0); // Reset after completing the sequence
-            }
+            
           } else {
+              this.currentPara[this.typed()] = `<span class="text-red-500">${this.currentKey[this.typed()] == ' ' ? '&nbsp' : this.currentKey[this.typed()].toLowerCase() }</span>`
+              this.typed.update(value => value +1); // Increment the signal.
               console.log("Incorrect key pressed")
-              this.typed.set(0); // Reset if wrong key is typed
+          }
+
+          if (this.typed() === this.currentKey.length) {
+            console.log('All keys typed correctly!');
+            this.typed.set(0); // Reset after completing the sequence
+          } else {
+            this.currentPara[this.typed()] = `<span class="blinking-border">${this.currentKey[this.typed()] == ' ' ? '&nbsp' : this.currentKey[this.typed()].toLowerCase() }</span>`
           }
       
           if (event.key === 'Enter' && event.ctrlKey) {
@@ -41,5 +54,6 @@ export class ParaType implements OnInit {
     }
 
     ngOnInit() {
+      this.currentPara[this.typed()] = `<span class="blinking-border">${this.currentKey[this.typed()] == ' ' ? '&nbsp' : this.currentKey[this.typed()].toLowerCase() }</span>`  
     }
 }
